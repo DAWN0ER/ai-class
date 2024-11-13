5b7c459d1475d626d6e9c512266f0cf061c0f94b
Dawn Yang <dawn112358@outlook.com>
2024-11-13 11:13:38 +0800

    智能体交互实验设计

--- code/pre_test/character.py
+++ code/pre_test/character.py
@@ -5,7 +5,9 @@ import random
 # 文件静态内容
 my_model = "qwen-long"
 mask = "█"
-clazz = dict()
+# 学生花名册 name:Agent
+roster = dict()
+teacher_list = []
 
 print("============加载分词器============")
 tokenizer = thulac.thulac(
@@ -14,6 +16,11 @@ tokenizer = thulac.thulac(
 )
 print("============加载完成============")
 
+extra_prompt = '''你需要做到的额外要求如下：
+强制要求：在交流的开头携带“[身份][姓名]”形式的前缀，表明身份和姓名，其他和你交流的角色也会带有相同的前缀。
+'''
+
+# 基类
 class Character:
 
     def __init__(self, anget:Agent, client:Swarm, forget_ratio:float, permanent:int):
@@ -21,14 +28,13 @@ class Character:
         self.__client = client
         self.forget_ratio = forget_ratio
         self.permanent = permanent
-        self.__memory = []
-        clazz[anget.name]=self
+        self.memory = []
     
-    def __forget(self):
-        if (len(self.__memory) == 0):
+    def forget(self):
+        if (len(self.memory) == 0):
             return
         cnt = 0
-        for msg in self.__memory :
+        for msg in self.memory :
             if (cnt > self.permanent):
                 break
             if (msg["role"] == "system"):
@@ -42,31 +48,37 @@ class Character:
             cnt += 1
 
     def __remember(self, msg):
-        self.__memory.append(msg)
+        self.memory.append(msg)
 
     def talk(self, content:str) -> str:
-        self.__forget()
         self.__remember({"role":"user","content":content})
-        rsp = self.__client.run(
-            agent=self.__agent,
-            messages=self.__memory
-        )
-        answer = rsp.messages[-1]["content"]
-        self.__remember({"role":"assistant","content":answer})
-        return answer
-    
-    def self_intro(self):
-        print(f"{self.__agent.name}! 到!")
+        try:
+            rsp = self.__client.run(
+                agent=self.__agent,
+                messages=self.memory
+            )
+            answer = rsp.messages[-1]["content"]
+            self.__remember({"role":"assistant","content":answer})
+            return answer
+        except Exception as e:
+            print(f"Character:{self.__agent.name}, 交流出现异常:{e}!")
+            return "抱歉出错了，我无法回答这个问题。"
 
+# 学生
 class Student(Character):
 
     def __init__(self, name:str, client:Swarm, forget_ratio:float, permanent:int):
         new_agent = Agent(
             name=name,
             model=my_model,
-            instructions = "你是一名学生", # TODO
-            functions=[],
-            tool_choice="",
+            # TODO
+            instructions = 
+                "你是一名学生，名字是" + name
+                + "如果你需要和班级里的其他学生交流时，可以使用 talk2 函数。"
+                + "如果你需要拿到班级中的学生的花名册，可以使用 aware_roster 函数。"
+                + "如果你需要询问老师问题，可以使用 ask_teacher 函数。"
+                + extra_prompt,
+            functions = [talk2,aware_roster,ask_teacher]
         )
         super().__init__(
             anget=new_agent,
@@ -74,16 +86,27 @@ class Student(Character):
             forget_ratio=forget_ratio,
             permanent=permanent
         )
+        # 学生加入花名册
+        roster[name]=self
+
+    def talk(self, content:str) -> str:
+        super().forget()
+        return super().talk(content=content)
     
+# 老师
 class Teacher(Character):
 
     def __init__(self, name:str, client:Swarm, forget_ratio:float, permanent:int):
         new_agent = Agent(
             name=name,
             model=my_model,
-            instructions = "你是一名老师", # TODO
-            functions=[],
-            tool_choice="",
+            # TODO
+            instructions = 
+                "你是一名老师，名字是" + name
+                + "如果你需要和班级里的其他学生交流时，可以使用 talk2 函数。"
+                + "如果你需要拿到班级中的学生的花名册，可以使用 aware_roster 函数。" 
+                + extra_prompt,
+            functions = [talk2,aware_roster],
         )
         super().__init__(
             anget=new_agent,
@@ -91,6 +114,50 @@ class Teacher(Character):
             forget_ratio=forget_ratio,
             permanent=permanent
         )
+        # 老师加入 list
+        teacher_list.append(self)
     
-def getClazz() -> dict:
-    return clazz
+# functions[]
+# 和其他角色交流的能力
+def talk2(name:str, content:str) -> str:
+    """
+    和班级里的其他学生交流的函数。
+
+    参数:
+    name(str): 想要交谈的人名字
+    content(str): 想要交谈的内容，强制要求开头携带“[身份][姓名]”形式的前缀，表明身份和姓名。
+
+    返回值:
+    str: 对方的回答
+    """
+
+    crct = roster[name]
+    if(crct == None):
+        return '名字出错了，没有找到这个人。'
+    return crct.talk(content)
+
+# 感知班级环境的能力：获取学生花名册
+def aware_roster() -> list:
+    """
+    拿到班级中的学生的花名册。
+    
+    返回值:
+    list: 班级所有学生姓名的列表
+    """
+
+    return list(roster.keys())
+
+def ask_teacher(content:str) -> str:
+    """
+    用于询问老师问题。
+    
+    参数: 
+    content(str): 想要询问的内容，强制要求开头携带“[身份][姓名]”形式的前缀，表明身份和姓名。
+    
+    返回值: 
+    str: 对方的回答。
+    """
+
+    if(teacher_list == []):
+        return "你所在的班级没有老师。"
+    return teacher_list[0].talk(content)
--- code/pre_test/classtest.py
+++ code/pre_test/classtest.py
@@ -1,15 +1,28 @@
-from character import Student,Teacher,getClazz
+from character import Student,Teacher
+from swarm import Swarm
+from openai import OpenAI
 
+client = Swarm(
+    client= OpenAI(
+        api_key="sk-59a6606f67394158a868a39dc6d2fe3d",
+        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    )
+)
 
-t1 = Teacher("T1",None,0.1,10)
-t2 = Teacher("T2",None,0.1,10)
-t3 = Teacher("T3",None,0.1,10)
-t4 = Teacher("T4",None,0.1,10)
+t = Teacher("张老师",client,0.1,10)
 
-s1 = Student("S1",None,0.1,10)
-s2 = Student("S2",None,0.1,10)
+s1 = Student("张三",client,0.1,10)
+s2 = Student("李四",client,0.1,10)
 
-c = getClazz()
+def tm():
+    print(f"Teacher 的 memory:{t.memory}")
+    print(f"张三 的 memory:{s1.memory}")
+    print(f"李四 的 memory:{s2.memory}")
 
-c["T1"].self_intro()
-c["T2"].self_intro()
+print("===== 实验开始 =====")
+tm()
+ans = t.talk("[管理员][Dawn]你们班有哪些学生，他们是怎么介绍自己的。")
+# ans = s1.talk("[管理员][Dawn]介绍一下你自己。")
+print(f"Answer:\n{ans}")
+print("=====对话结束=====")
+tm()
